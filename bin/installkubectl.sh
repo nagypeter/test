9cee4417722b2f8e576fd574117fd9cdab594501
Peter Nagy <>
2019-10-16 18:10:38 +0200

    1

--- bin/installkubectl.sh
+++ bin/installkubectl.sh
@@ -9,6 +9,8 @@ echo "Install kubectl..."
 
 sudo rm -rf /usr/local/bin/kubectl
 
+rm -rf ~/.kube
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
