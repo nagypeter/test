bb11a2e621b59cbde1977d26d4ae7eb8bae9fdb5
Peter Nagy <>
2019-10-16 18:19:38 +0200

    2

--- bin/installdocker.sh
+++ bin/installdocker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#usage:
+# curl -LSs https://raw.githubusercontent.com/nagypeter/test/master/bin/installdocker.sh | bash
+
+
 sudo yum -y remove docker-ce \
 	docker-ce-cli
 
@@ -16,9 +20,6 @@ sudo yum-config-manager \
 sudo yum -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 
 sudo yum -y install docker-ce-19.03.3 docker-ce-cli-19.03.3
-
-#ce 3:19.03.3-3.el7
-#cli 1:19.03.3-3.el7
 
 sudo systemctl enable docker
 
