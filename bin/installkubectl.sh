110f133e58922bbb680b15547a27a538995b21ea
Peter Nagy <>
2019-10-16 18:10:24 +0200

    no message

--- bin/installkubectl.sh
+++ bin/installkubectl.sh
@@ -3,13 +3,21 @@
 #usage:
 # curl -LSs https://raw.githubusercontent.com/nagypeter/test/master/bin/installkubectl.sh | sh
 
+sudo yum install bash-completion
+
 echo "Install kubectl..."
+
+sudo rm -rf /usr/local/bin/kubectl
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
 
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+
+. ~/.bashrc
 
 echo "========================================"
 
@@ -23,6 +31,6 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.s
 
 chmod 700 get_helm.sh
 
-./get_helm.sh # --version v2.8.2
+./get_helm.sh --version v2.8.2
 
 rm -rf helm*
