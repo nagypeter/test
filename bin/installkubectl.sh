ec5857362b5ff5cccf4623d0e6456c707f449e69
Peter Nagy <>
2019-10-16 17:22:00 +0200

    changes

--- bin/installkubectl.sh
+++ bin/installkubectl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#usage:
+# curl -LSs https://raw.githubusercontent.com/nagypeter/test/master/bin/installkubectl.sh | sh
+
 echo "Install kubectl..."
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
@@ -20,8 +23,6 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.s
 
 chmod 700 get_helm.sh
 
-./get_helm.sh --version v2.8.2
-
-helm init
+./get_helm.sh # --version v2.8.2
 
 rm -rf helm*
