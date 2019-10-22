c0a81223b6cb648ec90de81751965722664c0591
Peter Nagy <>
2019-10-22 14:52:27 +0200

    test

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#usage:
+# curl -LSs https://raw.githubusercontent.com/nagypeter/test/master/test.sh | bash
+
+echo "Setup environment for WebLogic Kubernetes Operator Workshop..."
+
+echo "do some stuff"
+
+read -p "Setup complete for WebLogic Kubernetes Operator Workshop. Press [Enter] to close the window"
